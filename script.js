b192e601e77a7e18c32af42b914b4c398106e0ee
Brian Wynn <brian.wynn83@gmail.com>
2015-01-11 01:21:43 -0800

    added boardType array to access and printed further data. added basic table to html to display content.

--- script.js
+++ script.js
@@ -8,14 +8,28 @@ function Wave(name, location, minHeight, maxHeight) {
   }
 }
 
+//setting global array for waveType
 var waveType = ['pointbreak', 'beachbreak', 'reefbreak'];
+var boardType = ['shortboard', 'longboard', 'fish', 'gun']
 
 var steamers = new Wave('Steamer Lane', 'Santa Cruz, CA.');
 var rincon = new Wave('Rincon', 'Carpenteria, CA.', 'pointbreak', 'longboard');
 var trestles = new Wave('Trestles', 'San Clemente, CA.', 'reefbreak', 'shortboard');
 
+//steamers details
 var details1 = steamers.name + ', ' + steamers.location;
-    details1 += steamers.bestSize();
-var details2 = 'Type of wave: ' + waveType[0];
-var wave1 = document.getElementById('wave1');
-wave1.textContent = details1 + details2;
+var details2 = waveType[0];
+var range1 = steamers.bestSize();
+var boards = boardType[0];
+
+var waveTitle = document.getElementById('waveTitle');
+waveTitle.textContent = details1;
+
+var waveRange = document.getElementById('waveRange');
+waveRange.textContent = range1;
+
+var waveType = document.getElementById('waveType');
+waveType.textContent = details2;
+
+var boardType = document.getElementById('boardType');
+boardType.textContent = boards;
