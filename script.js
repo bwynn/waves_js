1c8ee7c53af826bd33d404621ea54fcb016f5b25
Brian Wynn <brian.wynn83@gmail.com>
2015-01-11 01:07:07 -0800

    created bestSize function to designate wave size range

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
-function Wave(name, location, waveType, boardType) {
+function Wave(name, location, minHeight, maxHeight) {
   this.name = name;
   this.location = location;
-  var waveType = new Array['pointbreak'];
-  this.boardType = boardType;
+  this.bestSize = function() {
+    var minHeight = 4;
+    var maxHeight = 20;
+    return minHeight + ' to ' + maxHeight + ' feet';
+  }
 }
 
-var steamers = new Wave('Steamer Lane', 'Santa Cruz, CA.', waveType[0], 'shortboard');
+var waveType = ['pointbreak', 'beachbreak', 'reefbreak'];
+
+var steamers = new Wave('Steamer Lane', 'Santa Cruz, CA.');
 var rincon = new Wave('Rincon', 'Carpenteria, CA.', 'pointbreak', 'longboard');
 var trestles = new Wave('Trestles', 'San Clemente, CA.', 'reefbreak', 'shortboard');
 
 var details1 = steamers.name + ', ' + steamers.location;
-var details2 = 'Type of wave: ' + steamers.waveType;
+    details1 += steamers.bestSize();
+var details2 = 'Type of wave: ' + waveType[0];
 var wave1 = document.getElementById('wave1');
 wave1.textContent = details1 + details2;
