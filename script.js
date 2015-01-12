7772d84c6f9cdcf142fbe04c8e6ea051a756747f
Brian Wynn <brian.wynn83@gmail.com>
2015-01-11 16:51:41 -0800

    added WaveType function containing array to assign to different waves.

--- script.js
+++ script.js
@@ -2,23 +2,28 @@ function Wave(name, location, minHeight, maxHeight) {
   this.name = name;
   this.location = location;
   this.bestSize = function() {
-    var minHeight = 4;
-    var maxHeight = 20;
+    var minHeight = 4; //reduce to declaration, add values within Steamers function
+    var maxHeight = 20; // reduce to declaration, same as above
     return minHeight + ' to ' + maxHeight + ' feet';
   }
-}
+};
 
-//setting global array for waveType
-var waveType = ['pointbreak', 'beachbreak', 'reefbreak'];
-var boardType = ['shortboard', 'longboard', 'fish', 'gun']
+function WaveType() {
+  var waveArray = [
+    'pointbreak',
+    'beachbreak',
+    'reefbreak'
+  ];
+  return waveArray;
+};
 
+function Steamers() {
 var steamers = new Wave('Steamer Lane', 'Santa Cruz, CA.');
-var rincon = new Wave('Rincon', 'Carpenteria, CA.', 'pointbreak', 'longboard');
-var trestles = new Wave('Trestles', 'San Clemente, CA.', 'reefbreak', 'shortboard');
+var waveType1 = new WaveType();
+var boardType = ['shortboard', 'longboard', 'fish', 'gun'];
 
 //steamers details
 var details1 = steamers.name + ', ' + steamers.location;
-var details2 = waveType[0];
 var range1 = steamers.bestSize();
 var boards = boardType[0];
 
@@ -28,8 +33,14 @@ waveTitle.textContent = details1;
 var waveRange = document.getElementById('waveRange');
 waveRange.textContent = range1;
 
-var waveType = document.getElementById('waveType');
-waveType.textContent = details2;
+var waveKind = document.getElementById('waveType');
+console.log(waveType1[0]);
+waveKind.textContent = waveType1[0];
 
 var boardType = document.getElementById('boardType');
 boardType.textContent = boards;
+};
+
+//var rincon = new Wave('Rincon', 'Carpenteria, CA.', 'pointbreak', 'longboard');
+//var trestles = new Wave('Trestles', 'San Clemente, CA.', 'reefbreak', 'shortboard');
+Steamers();
