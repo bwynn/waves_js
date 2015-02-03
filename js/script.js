c1cd7e05f1b4e56e4031e548b7e417a48c7a0271
Brian Wynn <brian.wynn83@gmail.com>
2015-02-03 00:39:11 -0800

    linked up normalize and cleaned up page navigation call in script.js file.

--- js/script.js
+++ js/script.js
@@ -1,13 +1,14 @@
-$('nav a').on('click', function(e) {
-  e.preventDefault();
-  var url = this.href;
-  console.log('works');
+function pageNav() {
+  $('nav a').on('click', function(e) {
+    e.preventDefault();
+    var url = this.href;
 
-  $('nav a.active').removeClass('active');
-  $(this).addClass('active');
-  console.log('so');
+    $('nav a.active').removeClass('active');
+    $(this).addClass('active');
 
-  $('#content').remove();
-  $('#bodyWrap').load(url + ' #content').hide().fadeIn('slow');
-  console.log('far')
-});
+    $('#content').remove();
+    $('#bodyWrap').load(url + ' #content').hide().fadeIn('slow');
+  });
+}
+
+pageNav();
